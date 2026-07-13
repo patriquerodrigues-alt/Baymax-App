43c8d3bcb4e7c5b73048337fb4d5a6bc0dc59fde
patriquerodrigues-alt <patrique.rodrigues@saipos.com>
2026-07-13 10:03:42 -0300

    Fix sw.js cache list (baymax.html -> index.html) and bump cache version

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'baymax-cache-v1';
+const CACHE_NAME = 'baymax-cache-v2';
 const FILES_TO_CACHE = [
-  './baymax.html',
+  './index.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
